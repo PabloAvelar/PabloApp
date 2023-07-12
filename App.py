d069e00b091f6431e829fa80615d3428ea6dd395
PabloAvelar <pabloarmenta84@gmail.com>
2023-07-12 13:56:12 -0600

    Adding a window menu, making homogeneous this app, and an entry field but with no button to download the content.

--- App.py
+++ App.py
@@ -18,43 +18,7 @@ import os
 
 class App:
 
-    @property
-    def path(cls) -> str:
-        """ Devuelve: la ruta donde se descargan los videos -> str"""
-
-        directory = os.getcwd()+"\\Downloads" # Es el directorio por defecto
-        with open(os.getcwd()+"\\Downloads\\dir.txt", "w+") as f:
-            if len(f.read) == 0:
-                # Si no hay nada, se pone el directorio por defecto
-                # Esto se suele hacer cuando es la primera vez que se abre el programa.
-                f.write(directory)
-            else:
-                # Si ya tenemos el directorio en un archivo, cambiamos el valor
-                # de "directory", que será lo que devolverá esta función.
-                directory = f.read()
-            
-            f.close()
-
-        return directory
     
-    @path.setter
-    def path(self, new_path) -> None:
-        """ Reescribe la nueva ruta para los videos descargados 
-
-            Recibe: filedialog.askdirectory() -> str
-            Devuelve: nada
-        """
-        if len(new_path) > 0:
-            with open(os.getcwd()+"\\Downloads\\dir.txt", "w") as f:
-                f.write(new_path)
-                f.close()
-
-    @staticmethod
-    def open_in_explorer() -> None:
-        """ Abre el directorio en el explorador de archivos """
-        os.startfile(App.path)
-
-
     def download(link:str) -> bool:
         """
 
--- pabloapp.py
+++ pabloapp.py
@@ -25,9 +25,10 @@ la aplicación, sin cerrar procesos o algo así. Así sin cerrar ventanas y eso
 
 import tkinter as tk
 import SocialMedia as sm
+import os
 
 class UI(tk.Tk):
-    app = None
+    bg_color = "#1e1e1e"
     def __init__(self):
         super().__init__()
         
@@ -35,12 +36,6 @@ class UI(tk.Tk):
         self.h_win = 600
         _x_win = (self.winfo_screenwidth() // 2) - (self.w_win//2)
         _y_win = (self.winfo_screenheight() // 2) - (self.h_win//2)
-        
-        self.bg = {
-            "fb": "#3b5999",
-            "ig": "#e4405f",
-            "yt": "#cd201f"
-        }
         
         # Recursos #
         self.resources = {
@@ -50,7 +45,6 @@ class UI(tk.Tk):
             "logo_yt": tk.PhotoImage(file='img/redes/ytlogo.png'),
             "open": tk.PhotoImage(file='img/botones/abrir.png'),
             "download": tk.PhotoImage(file='img/botones/descargar.png'),
-            "change": tk.PhotoImage(file='img/botones/cambiar.png'),
             "socialmedia": tk.PhotoImage(file='img/botones/redes.png')
         }
         
@@ -59,106 +53,125 @@ class UI(tk.Tk):
         self.iconbitmap('img/logo/icono_app.ico')
         self.title('PabloApp')
         
-        UI.selectApp()
-        self.config(bg=self.bg[UI.app])
+        self.config(bg=UI.bg_color)
         
+        # Creando el menú de la ventana
+        main_menu = tk.Menu(self)
+        self.config(menu=main_menu)
+
+        # Le ponemos una sección y lo asignamos al menú de la ventana
+        tools = tk.Menu(main_menu, tearoff=0)
+        main_menu.add_cascade(label="Archivo", menu=tools) # En formato de cascada!
+
+        # Creamos elementos para ese menú
+        tools.add_command(label="Abrir ruta de descargas", command=lambda:self.open_in_explorer())
+        tools.add_command(label="Cambiar ruta de descargas", command=lambda:print("Cambiar ruta"))
+        tools.add_command(label="Acerca de", command=lambda:print("Acerca de"))
+
         self.showApps()
-        
-    @classmethod
-    def selectApp(cls, next_app="fb") -> None:
+        self.showForm()
+
+    
+    @property
+    def path(self) -> str:
+        """ Devuelve: la ruta donde se descargan los videos -> str"""
+        directory = os.getcwd()+"\\Downloads" # Es el directorio por defecto DONDE ESTÁ UBICADO EL PROGRAMA
+        with open(os.getcwd()+"\\dir\\dir.txt", "w+") as f:
+            if len(f.read()) == 0:
+                # Si no hay nada, se pone el directorio por defecto
+                # Esto se suele hacer cuando es la primera vez que se abre el programa.
+                f.write(directory)
+            else:
+                # Si ya tenemos el directorio en un archivo, cambiamos el valor
+                # de "directory", que será lo que devolverá esta función.
+                directory = f.read()
+            
+            f.close()
+        return directory
+    
+    @path.setter
+    def path(self, new_path) -> None:
+        """ Reescribe la nueva ruta para los videos descargados 
+
+            Recibe: filedialog.askdirectory() -> str
+            Devuelve: nada
         """
-        Se encarga de cambiar de app:
-            llama a las instancias de fb, ig, yt
-            cambia la variable de clase        
+        if len(new_path) > 0:
+            with open(os.getcwd()+"\\Downloads\\dir.txt", "w") as f:
+                f.write(new_path)
+                f.close()
+
+    def open_in_explorer(self) -> None:
+        """ Abre el directorio en el explorador de archivos """
+        os.startfile(self.path)
+
+
+    def showForm(self) -> None:
+        """
+            Muestra en la interfaz gráfica un campo de texto y un botón
+            para que el usuario ingrese el link de su video y pueda
+            iniciar su descarga.
+
+            Aquí se detectará si el link es de:
+                -Facebook
+                -Instagram
+                -YouTube
+
         """
 
-        # Para que no abra la misma app en la que está actualmente
-        # Así en el autómata no tenemos lazos en la interfaz.
-        if cls.app != next_app:
-            cls.app = next_app
-            if cls.app == "fb":
-                print(sm.Facebook())
-            elif cls.app == "ig":
-                print(sm.Instagram())
-            elif cls.app == "yt":
-                print(sm.YouTube())
+        # Le damos forma al frame del formulario
+        _x = self.w_win // 4 
+        _y = self.h_win // 10
 
-    def changeInterface(self, app:str) -> None:
-        """
-        
-        Parameters
-        ----------
-        app : str
-            Indica la aplicación a la que se cambiará el usuario.
-            Puede ser:
-                - YouTube -> yt
-                - Instagram -> ig
-                - Facebook -> fb
+        form = tk.Frame(self)
+        form.config(bg=UI.bg_color, width=self.w_win, height=_y*4.2) #bg=UI.bg_color
+        form.pack(padx=_x, pady=_y)
 
-        Returns
-        -------
-        None
-            Cambia el color de la interfaz y usa ahora el módulo
-            correspondiente a la aplicación a la que se cambió.
+        # Creamos el campo de texto para que el usuario ingrese el link
+        url = tk.StringVar()
+        entry = tk.Entry(form, textvariable=url)
+        entry.config(width=int(self.w_win*0.8), font=("Georgia", 15))
+        entry.pack(pady=_y*2)
 
-        """
-        # Cambiando la aplicación
-        UI.selectApp(app)
+        # Creamos un botón para que el usuario comience su descarga
         
-        # Cambiando el color del fondo de la interfaz gráfica
-        self.config(bg=self.bg[app])
-        
-        # Cambiando el color de fondo a cada widget de la interfaz grafica
-        for widget in self.winfo_children():
-            widget.config(bg=self.bg[app])
-        
+
     def showApps(self) -> None:
         """
-
+            Muestra en la interfaz gráfica los botones
+            para cambiar de aplicación.
         Returns
         -------
         None
-            Muestra en la interfaz gráfica los botones
-            para cambiar de aplicación.
-
         """
         
-        _background = self.bg[UI.app]
         _x = self.w_win * 0.04
         _y = self.h_win / 3
         
         self.header = tk.Label(self,
                  image=self.resources["header"],
-                 bg=_background)
+                 bg=UI.bg_color)
         self.header.pack()
-        
         
         self.fb_app = tk.Label(self,
                                image=self.resources["logo_fb"], 
-                               bg=_background
+                               bg=UI.bg_color
                                )
-        self.fb_app.config(cursor="hand2")
         self.fb_app.place(x=_x, y=_y)
 
         self.ig_app = tk.Label(self,
                                image=self.resources["logo_ig"],
-                               bg=_background
+                               bg=UI.bg_color
                                )
-        self.ig_app.config(cursor="hand2")
         _y += 100
         self.ig_app.place(x=_x, y = _y)
 
         self.yt_app = tk.Label(self,
                                image=self.resources["logo_yt"],
-                               bg=_background
+                               bg=UI.bg_color
                                )
-        self.yt_app.config(cursor="hand2")
         _y += 100
         self.yt_app.place(x=_x, y = _y)
-        
-        self.fb_app.bind('<Button-1>', lambda _:self.changeInterface('fb'))
-        self.ig_app.bind('<Button-1>', lambda _:self.changeInterface('ig'))
-        self.yt_app.bind('<Button-1>', lambda _:self.changeInterface('yt'))
         
             
 if __name__ == '__main__':
