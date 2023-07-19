30444f15e23c2e7b7b456e46bfc9b19031ee77f9
PabloAvelar <pabloarmenta84@gmail.com>
2023-07-19 00:13:41 -0600

    I made a class for every frame in the UI and now it works!

--- pabloapp.py
+++ pabloapp.py
@@ -24,13 +24,13 @@ la aplicación, sin cerrar procesos o algo así. Así sin cerrar ventanas y eso
 """
 
 import tkinter as tk
+from tkinter import ttk
 import SocialMedia as sm
 import os
 
 class UI(tk.Tk):
-    bg_color = "#1e1e1e"
-    frame_color = "#292929"
     def __init__(self, title, size):
+        # Main setup
         super().__init__()
 
         _x_win = (self.winfo_screenwidth() // 2) - (size[0]//2)
@@ -40,6 +40,9 @@ class UI(tk.Tk):
         self.geometry(f'{size[0]}x{size[1]}+{_x_win}+{_y_win}')
         
         # Recursos #
+        self.bg_color = "#1e1e1e"
+        self.frame_color = "#292929"
+
         self.resources = {
             "header": tk.PhotoImage(file='img/logo/pabloapp_texto.png'),
             "logo_fb": tk.PhotoImage(file='img/redes/fblogo.png'),
@@ -55,25 +58,28 @@ class UI(tk.Tk):
         self.iconbitmap('img/logo/icono_app.ico')
         
         
-        self.config(bg=UI.bg_color)
-        
         # Creando el menú de la ventana
-        main_menu = tk.Menu(self)
-        self.config(menu=main_menu)
+        self.menu = Menu(self)
 
-        # Le ponemos una sección y lo asignamos al menú de la ventana
-        tools = tk.Menu(main_menu, tearoff=0)
-        main_menu.add_cascade(label="Configuración", menu=tools) # En formato de cascada!
+        # Configuraciones
+        self.config(bg=self.bg_color, menu=self.menu)
 
-        # Creamos elementos para ese menú
-        tools.add_command(label="Abrir ruta de descargas", command=lambda:self.open_in_explorer())
-        tools.add_command(label="Cambiar ruta de descargas", command=lambda:print("Cambiar ruta"))
-        tools.add_command(label="Acerca de", command=lambda:print("Acerca de"))
+        # El header de la app
+        self.header = Header(self)
 
-        self.showHeader()
-        self.showApps()
-        self.showForm()
-        self.showFooter()
+        # Formulario para descargar el contenido
+        self.form = Form(self)
+
+        # El footer de la app
+        self.footer = Footer(self)
+
+        # El frame de la derecha con las apps
+        self.apps = Apps(self)
+
+        # Arrancar
+        self.mainloop()
+
+        
 
     
     @property
@@ -109,104 +115,120 @@ class UI(tk.Tk):
         """ Abre el directorio en el explorador de archivos """
         os.startfile(self.path)
 
+class Menu(tk.Menu):
+    """
+    Este es el menú desplegable de la ventana
+    """
 
-    def showForm(self) -> None:
-        """
-            Muestra en la interfaz gráfica un campo de texto y un botón
-            para que el usuario ingrese el link de su video y pueda
-            iniciar su descarga.
+    def __init__(self, parent):
+        super().__init__(parent)
+        
+        # Creando secciones del menú
+        self.create_section()
+        
+    def create_section(self):
+        # Asignamos una sección con cascada
+        tools = tk.Menu(self, tearoff=0)
+        self.add_cascade(label="Configuración", menu=tools)
+        
+        # Creamos elementos para ese menú
+        tools.add_command(label="Abrir ruta de descargas", command=lambda:self.open_in_explorer())
+        tools.add_command(label="Cambiar ruta de descargas", command=lambda:print("Cambiar ruta"))
+        tools.add_command(label="Acerca de", command=lambda:print("Acerca de"))
 
-            Aquí se detectará si el link es de:
-                -Facebook
-                -Instagram
-                -YouTube
 
-        """
 
-        # Le damos forma al frame del formulario
-        _x = self.w_win // 4 
-        _y = self.h_win // 10
+class Form(ttk.Frame):
+    """
+    Muestra en la interfaz gráfica un campo de texto y un botón
+    para que el usuario ingrese el link de su video y pueda
+    iniciar su descarga.
 
-        form = tk.Frame(self)
-        form.config(bg=UI.frame_color) #bg=UI.bg_color, , width=150, height=_y*4.2
-        # form.place(relx=0.5, rely=0.5, relwidth=0.5, relheight=0.15, anchor="c")
-        # form.pack(pady=self.h_win//4)
-        form.grid(row=1, column=1)
+    Aquí se detectará si el link es de:
+    -Facebook
+    -Instagram
+    -YouTube
 
+    """
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        # self.config(bg=parent.frame_color) #bg=parent.bg_color, , width=150, height=_y*4.2
+        # self.place(relx=0.5, rely=0.5, relwidth=0.5, relheight=0.15, anchor="c")
+        self.grid(row=1, column=1)
+        
+        self.create_widgets()
+
+    def create_widgets(self):
         # Creamos el campo de texto para que el usuario ingrese el link
         url = tk.StringVar()
-        entry = tk.Entry(form, textvariable=url)
+        entry = tk.Entry(self, textvariable=url)
         entry.config(width=50, font=("Open Sans", 12), borderwidth=10, relief=tk.FLAT) #int((self.w_win*0.05))
-        # entry.place(relx=0.5, rely=0.5, anchor="c")
         entry.grid(row=1, column=1, columnspan=3, padx=15, pady=20)
-        # entry.pack(pady=_y*2)
 
         # Creamos un botón para que el usuario comience su descarga
         def thing():
             pass
         img_download = tk.PhotoImage(file='img/botones/online/download.png')
-        btn_download = tk.Button(form, image=img_download, command=thing)
-        btn_download.config(borderwidth=0, bg=UI.frame_color, activebackground=UI.frame_color, cursor="hand2")
-        # btn_download.place(relx=0.5, rely=0.5, anchor="c")
+        btn_download = tk.Button(self, image=img_download, command=thing)
+        btn_download.config(borderwidth=0, bg=self.parent.frame_color, activebackground=self.parent.frame_color, cursor="hand2")
         btn_download.image = img_download
         btn_download.grid(row=2, column=2, pady=10)
-        # btn_download.pack()
 
-    def showApps(self) -> None:
-        """
-            Muestra en la interfaz gráfica los botones
-            para cambiar de aplicación.
-        Returns
-        -------
-        None
-        """
+class Apps(ttk.Frame):
+    """
+    Muestra en la interfaz gráfica los botones
+    para cambiar de aplicación.
+    Returns
+    -------
+    None
+    """
+    def __init__(self, parent):
+        super().__init__(parent)
+        # self.config(bg=parent.frame_color)
+        self.grid(row=1, column=0)
+
+        _x = 1_000 * 0.04
+        _y = 600 / 3
         
-        apps = tk.Frame(self)
-        apps.config(bg=UI.frame_color)
-        apps.grid(row=1, column=0)
-
-        _x = self.w_win * 0.04
-        _y = self.h_win / 3
-        
-        self.fb_app = tk.Label(self,
-                               image=self.resources["logo_fb"], 
-                               bg=UI.bg_color
+        fb_app = tk.Label(self,
+                               image=parent.resources["logo_fb"], 
+                               bg=parent.bg_color
                                )
-        self.fb_app.place(x=_x, y=_y)
+        fb_app.place(x=_x, y=_y)
 
-        self.ig_app = tk.Label(self,
-                               image=self.resources["logo_ig"],
-                               bg=UI.bg_color
-                               )
-        _y += 100
-        self.ig_app.place(x=_x, y = _y)
-
-        self.yt_app = tk.Label(self,
-                               image=self.resources["logo_yt"],
-                               bg=UI.bg_color
+        ig_app = tk.Label(self,
+                               image=parent.resources["logo_ig"],
+                               bg=parent.bg_color
                                )
         _y += 100
-        self.yt_app.place(x=_x, y = _y)
-        
+        ig_app.place(x=_x, y = _y)
 
-    def showHeader(self):
-        header = tk.Frame(self)
-        header.grid(row=0, column=1)
+        yt_app = tk.Label(self,
+                               image=parent.resources["logo_yt"],
+                               bg=parent.bg_color
+                               )
+        _y += 100
+        yt_app.place(x=_x, y = _y)
 
-        logo = tk.Label(header,
-                 image=self.resources["header"],
-                 bg=UI.bg_color)
+class Header(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.grid(row=0, column=1)
+        logo = tk.Label(self,
+                 image=parent.resources["header"],
+                 bg=parent.bg_color)
         logo.pack()
 
-    def showFooter(self):
-        footer = tk.Frame(self)
-        footer.config(bg=UI.frame_color)
-        footer.grid(row=2, column=1, columnspan=3)
+class Footer(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
 
-        tk.Label(footer, text="hola").pack()
+        # self.config(bg=parent.frame_color)
+        self.grid(row=2, column=1, columnspan=3)
+
+        tk.Label(self, text="hola").pack()
 
 if __name__ == '__main__':
-    root = UI('PabloApp', (600, 1_000))
-    root.mainloop()
-        
-        
+    UI('PabloApp', (1_000, 600))
